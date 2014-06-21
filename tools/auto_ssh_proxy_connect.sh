770d925b9931551068c4ffd708a2e33635a6372c
jason0730 <quanzhenglong@gmail.com>
2014-06-21 17:17:11 +0800

    添加脚本

---
+++ tools/auto_ssh_proxy_connect.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+http_status_code=`curl -I 'https://www.youtube.com' -m 5 --socks5-hostname 127.0.0.1:$SSH_PROXY_PORT 2>/dev/null | head -1 | cut -d ' ' -f2`
+
+if [[ $http_status_code != '200' ]];then
+    ssh_proxy stop 2>/dev/null
+    ssh_proxy start $SSH_PROXY_HOSTNAME $SSH_SERVER_PORT 2>/dev/null
+fi
