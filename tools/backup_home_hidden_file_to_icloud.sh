82fd82188fb74eeada2e6f0fa6446a187f10088d
权政龙 <quanzhenglong@MacBook-Pro.local>
2017-12-06 16:15:04 +0800

    添加备份配置到icloud的脚本

---
+++ tools/backup_home_hidden_file_to_icloud.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function move_hidden_files_to_icloud() {
+    local filename=$1
+    local icloudpath=$2
+    local shortname=`echo -n $filename | cut -d '.' -f2-`
+
+    test ! -L "$HOME/$filename" && test ! -e "$icloudpath/$shortname" && mv -v "$HOME/$filename" "$icloudpath/$shortname" && echo "move done"
+}
+
+function link_icloud_dir_to_home_hidden_file() {
+    local shortname=$1
+    local icloudpath=$2
+    test ! -e "$HOME/.$shortname" && ln -v -s  "$icloudpath/$shortname" "$HOME/.$shortname" && echo "create link done"
+}
+
+[ -z $1 ] && echo "please input directory name" && exit 1
+
+icloudpath="$HOME/Library/Mobile Documents/com~apple~CloudDocs/$1"
+test ! -d "$icloudpath" && echo "[$icloudpath] no such Directory" && exit 1
+
+ls -a $HOME/ | grep '^\.' | grep -E -v  '^\.+$' | while read filename
+do
+    move_hidden_files_to_icloud "$filename" "$icloudpath"
+done
+
+ls "$icloudpath" | while read shortname
+do
+    link_icloud_dir_to_home_hidden_file "$shortname" "$icloudpath"
+done
