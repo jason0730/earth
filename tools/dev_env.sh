725a5efa9fe4b1e3e489210d7794be00c0743957
权政龙 <quanzhenglong@MacBook-Pro.local>
2017-12-06 11:35:19 +0800

    test

--- tools/dev_env.sh
+++ tools/dev_env.sh
@@ -3,7 +3,7 @@
 open /Applications/Evernote.app/
 open /Applications/QQ.app/
 open /Applications/Google\ Chrome.app/
-open /Applications/Foxmail.app/
+#open /Applications/Foxmail.app/
 #open /Applications/Reeder.app/
 #open /Applications/RTX.app/
 #open /Applications/Pocket.app/
